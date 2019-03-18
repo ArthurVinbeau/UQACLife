0a991bb7979af7494e005bcd218989bcf62ddae4
Theo Golgevit <theo.golgevit@epita.fr>
2019-03-18 11:20:17 -0400

    login foundation

---
+++ app/src/main/java/uqac/dim/uqaclife/login.java
@@ -0,0 +1,18 @@
+package uqac.dim.uqaclife;
+
+public class login {
+
+    public String login(String id, String password, String captcha) {
+
+
+
+        return "";
+    }
+
+    public String getHTML(String cookie) {
+
+
+        return "";
+    }
+
+}
