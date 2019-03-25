63229ede8e19290f68f39c1a0919112384ac4a5e
Theo Golgevit <theo.golgevit@epita.fr>
2019-03-25 18:52:50 -0400

    link

--- app/src/main/java/uqac/dim/uqaclife/LoginActivity.java
+++ app/src/main/java/uqac/dim/uqaclife/LoginActivity.java
@@ -3,13 +3,17 @@ package uqac.dim.uqaclife;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
+import com.android.volley.toolbox.NetworkImageView;
+
 public class LoginActivity extends  MainActivity{
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        login.getCaptcha((NetworkImageView) findViewById(R.id.container_captcha));
 
     }
 }
