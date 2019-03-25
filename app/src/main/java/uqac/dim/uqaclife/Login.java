6645d4397d9a135b0b9859c50bba6d59d9f8a398
Theo Golgevit <theo.golgevit@epita.fr>
2019-03-25 15:08:34 -0400

    Captcha and login working

--- app/src/main/java/uqac/dim/uqaclife/Login.java
+++ app/src/main/java/uqac/dim/uqaclife/Login.java
@@ -1,140 +1,146 @@
 package uqac.dim.uqaclife;
 
+import android.app.DownloadManager;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
 import android.util.Log;
-import android.view.View;
-import android.widget.ImageView;
+import android.util.LruCache;
 import android.widget.TextView;
 
-import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
 import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 
-import com.google.gson.Gson;
-
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLConnection;
-import java.nio.charset.Charset;
-import java.util.stream.Collectors;
-
-import javax.net.ssl.HttpsURLConnection;
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class Login {
 
     private RequestQueue queue;
-    private Gson gson = new Gson();
+    private ImageLoader iLoader;
+    private CookieManager cookieManager;
 
     public void setQueue(RequestQueue q) {
         queue = q;
+        cookieManager = new CookieManager();
+        CookieHandler.setDefault(cookieManager);
+        Log.i("request", cookieManager.getCookieStore().getCookies().toString());
+        iLoader = new ImageLoader(queue, new ImageLoader.ImageCache() {
+            private final LruCache<String, Bitmap> mCache = new LruCache<String, Bitmap>(10);
+
+            public void putBitmap(String url, Bitmap bitmap) {
+                mCache.put(url, bitmap);
+            }
+
+            public Bitmap getBitmap(String url) {
+                return mCache.get(url);
+            }
+        });
     }
 
-    public void getCaptcha(final ImageView captchaContainer, final TextView txtField) {
+    public void getCaptcha(final NetworkImageView captchaContainer) {
 
         String url = "https://wprodl.uqac.ca/dossier_etudiant/";
 
-        /*// Request a string response from the provided URL.
+        Map<String, String> headers = new LinkedHashMap<>();
+
+
+        // Request a string response from the provided URL.
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-            new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response) {
-                    // Display the first 500 characters of the response string.
-                    //Log.i("request", "Response is: " + response.substring(500));
-                    String captcha = "https://wprodl.uqac.ca" + response.substring(response.indexOf("/commun/"), response.indexOf("\' alt=\"CAPTCHA\""));
-                    Log.i("request", captcha);
-                    Log.i("request", "Cookie: ");
-                    String fleur = "https://cdn.pixabay.com/photo/2018/02/09/21/46/rose-3142529_960_720.jpg";
-                    new DownloadImageTask(captchaContainer).execute(fleur, "cookie");
-                    txtField.setText(response);
-                }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Log.i("request","That didn't work!");
-                    Log.e("request", error.toString());
-                }
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        //Log.i("request", "Response is: " + response.substring(500));
+                        String captcha = "https://wprodl.uqac.ca" + response.substring(response.indexOf("/commun/"), response.indexOf("\' alt=\"CAPTCHA\"")) + "&cookie=" + "hpn201jdp2qmsi3cl89u3l00i1";
+                        Log.i("request", captcha);
+                        Log.i("request", "Cookie: " + cookieManager.getCookieStore().getCookies().toString());
+                        captchaContainer.setImageUrl(captcha, iLoader);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i("request", "That didn't work!");
+                Log.e("request", error.toString());
+            }
         });
 
         // Add the request to the RequestQueue.
-        queue.add(stringRequest);*/
+        queue.add(stringRequest);
 
-
-        try {
-            HttpsURLConnection urlConnection = (HttpsURLConnection) new URL(url).openConnection();
-            urlConnection.connect();
-            Log.i("request", urlConnection.getContentType());
-            /*InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-            String s = convert(in);
-            Log.i("request", s);*/
-            //Log.i("request", (urlConnection.getHeaderField("cookie")));
-            //Log.i("request", (urlConnection.toString()));
-        } catch (Exception e) {
-            Log.e("request", e.toString());
-            e.printStackTrace();
-        }
     }
 
-    public String convert(InputStream inputStream) throws IOException {
-
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
-            return br.lines().collect(Collectors.joining(System.lineSeparator()));
-        }
-    }
-
-    public String login(String id, String password, String captcha) {
+    public void Login(final String login, final String password, final String captcha, final TextView txt) {
+        String url = "https://wprodl.uqac.ca/dossier_etudiant/validation.html";
+        String url2 = "https://wprodl.uqac.ca/dossier_etudiant/grille_horaire.html?type=gl&session=TRIMESTRE";
 
 
-        return "";
-    }
+        final StringRequest stringRequest = new StringRequest(Request.Method.GET, url2,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.i("request", response);
+                        txt.setText(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.i("request", "That didn't work!");
+                        Log.e("request", error.toString());
+                    }
+                });
 
-    public String getHTML(String cookie) {
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        // response
+                        Log.i("request", response);
+                        queue.add(stringRequest);
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        Log.i("request", "That didn't work!");
+                        Log.e("request", error.toString());
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("codeSecret", password);
+                params.put("user", login);
+                params.put("codeSecret1", "12345678");
+                params.put("noCaptcha", captcha);
+                params.put("valider", "valider");
 
-
-        return "";
-    }
-
-    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
-        ImageView bmImage;
-
-        public DownloadImageTask(ImageView bmImage) {
-            this.bmImage = bmImage;
-        }
-
-        protected Bitmap doInBackground(String... urls) {
-            String urldisplay = urls[0];
-            String cookie = urls[1];
-            Bitmap mIcon11 = null;
-            try {
-                //InputStream in = new java.net.URL(urldisplay).openStream();
-                URLConnection con = new URL(urldisplay).openConnection();
-                con.setRequestProperty("Cookie", cookie);
-                con.connect();
-                InputStream in = con.getInputStream();
-                mIcon11 = BitmapFactory.decodeStream(in);
-            } catch (Exception e) {
-                Log.e("request", e.getMessage());
-                e.printStackTrace();
+                return params;
             }
-            return mIcon11;
-        }
 
-        protected void onPostExecute(Bitmap result) {
-            Log.i("request", "Setting image...");
-            bmImage.setImageBitmap(result);
-        }
+            @Override
+            public Map getHeaders() {
+                HashMap headers = new HashMap();
+                headers.put("Content-Type", "application/x-www-form-urlencoded");
+                headers.put("Referer", "application/x-www-form-urlencoded");
+
+                return headers;
+            }
+        };
+
+
+        queue.add(postRequest);
     }
-
 }
