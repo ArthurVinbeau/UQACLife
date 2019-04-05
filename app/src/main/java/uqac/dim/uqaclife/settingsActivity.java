3184a7742e412bbcbe0be578c7045f4633788b43
Mahe Charpy <mahe.charpy@epita.fr>
2019-04-05 12:49:30 -0400

    Removed onclick on day bar
    Signed-off-by: Mahe Charpy <mahe.charpy@epita.fr>

--- app/src/main/java/uqac/dim/uqaclife/settingsActivity.java
+++ app/src/main/java/uqac/dim/uqaclife/settingsActivity.java
@@ -1,13 +1,26 @@
 package uqac.dim.uqaclife;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class settingsActivity extends MainActivity {
+    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setttings);
         sharedPref = getSharedPreferences(getResources().getString(R.string.preferences_file), MODE_PRIVATE);
+        findViewById(R.id.Logout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sharedPref.edit().putString("login",null).apply();
+                sharedPref.edit().putString("password",null).apply();
+            }
+        });
+
     }
+
+
 }
